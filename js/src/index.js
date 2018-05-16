caa53e204d807e543bdc05723b04bc80cdb8af16
rodrigo-morais <morais.rm@gmail.com>
2018-05-16 18:05:32 +0200

    Refactoring. Split code in small functions to make readble.

--- js/src/index.js
+++ js/src/index.js
@@ -17,53 +17,51 @@ const create_item = (
   multiplier
 }) 
 
-const update_quality = function (items = []) {
-  return items.map(item => {
-    if (item.degrade_quality === '-') {
-      if (item.quality > MIN_QUALITY_LIMIT) {
-        if (item.degrade) {
-          item.quality = item.quality - item.multiplier
-        }
-      }
-    } else {
-      if (item.quality < MAX_QUALITY_LIMIT) {
-        item.quality = item.quality + 1
-        if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (item.sell_in < 11) {
-            if (item.quality < MAX_QUALITY_LIMIT) {
-              item.quality = item.quality + 1
-            }
-          }
-          if (item.sell_in < 6) {
-            if (item.quality < MAX_QUALITY_LIMIT) {
-              item.quality = item.quality + 1
-            }
-          }
-        }
-      }
-    }
-    if (item.degrade) {
-      item.sell_in = item.sell_in - 1;
-    }
-    if (item.sell_in < MIN_SELL_IN_LIMIT) {
-      if (item.name != 'Aged Brie') {
-        if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (item.quality > MIN_QUALITY_LIMIT) {
-            if (item.degrade) {
-              item.quality = item.quality - item.multiplier
-            }
-          }
-        } else {
-          item.quality = item.quality - item.quality
-        }
-      } else {
+const degrade_quality = item => {
+  const MULTIPLIER = item.sell_in > MIN_SELL_IN_LIMIT ? item.multiplier : item.multiplier * 2
+  return Object.assign({}, item, { quality: item.quality - MULTIPLIER < 0 ? 0 : item.quality - MULTIPLIER })
+}
+
+const upgrade_quality = item => {
+  if (item.quality < MAX_QUALITY_LIMIT) {
+    item.quality = item.quality + 1
+
+    if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+      if (item.sell_in < 11) {
         if (item.quality < MAX_QUALITY_LIMIT) {
           item.quality = item.quality + 1
         }
       }
+      if (item.sell_in < 6) {
+        if (item.quality < MAX_QUALITY_LIMIT) {
+          item.quality = item.quality + 1
+        }
+      }
+      if (item.sell_in < MIN_SELL_IN_LIMIT) {
+        item.quality = item.quality - item.quality
+      }
     }
-    return item;
-  }, []);
+    if (item.name === 'Aged Brie' &&
+      item.quality < MAX_QUALITY_LIMIT) {
+      item.quality = item.quality + 1
+    }
+  }
+
+  return item
 }
+
+const degrade_sell_in = item => Object.assign({}, item, { sell_in: item.sell_in - 1 })
+
+const update_item = item => {
+  switch (item.degrade_quality) {
+    case '+':
+      return upgrade_quality(degrade_sell_in(item))
+    default:
+      return degrade_quality(degrade_sell_in(item))
+  }
+}
+
+const update_quality = (items = []) =>
+  items.map(item => item.degrade ? update_item(item) : item, [])
 
 module.exports = { create_item, update_quality };
