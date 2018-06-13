5832e20715b83a97267ce4534902d349136a0fba
rodrigo-morais <morais.rm@gmail.com>
2018-06-13 12:00:39 +0200

    Adding constants to keep the code simple.

--- clojure/spec/gilded_rose/core_spec.clj
+++ clojure/spec/gilded_rose/core_spec.clj
@@ -1,6 +1,7 @@
 (ns gilded-rose.core-spec
 (:require [clojure.test :refer :all]
           [speclj.core :refer :all]
+          [gilded-rose.constants :refer :all]
           [gilded-rose.core :refer :all]))
 
 (describe "Gilded Rose"
@@ -24,9 +25,9 @@
       )
       (describe "when quality is the maximum limit"
         (it "returns less one of quality and sell by date"
-          (let [item-1 (item "Item 1" 10 50)
+          (let [item-1 (item "Item 1" 10 QUALITY_MAX_LIMIT)
                 updated-items (update-quality [item-1])]
-            (should= {:name "Item 1" :sell-in 9 :quality 49} (first updated-items)))
+            (should= {:name "Item 1" :sell-in 9 :quality (- QUALITY_MAX_LIMIT 1)} (first updated-items)))
         )
       )
     )
@@ -47,9 +48,9 @@
       )
       (describe "when quality is the maximum limit"
         (it "returns less two of quality and less one of sell by date"
-          (let [item-1 (item "Item 1" 0 50)
+          (let [item-1 (item "Item 1" 0 QUALITY_MAX_LIMIT)
                 updated-items (update-quality [item-1])]
-            (should= {:name "Item 1" :sell-in -1 :quality 48} (first updated-items)))
+            (should= {:name "Item 1" :sell-in -1 :quality (- QUALITY_MAX_LIMIT 2)} (first updated-items)))
         )
       )
     )
@@ -57,203 +58,203 @@
   (describe "Sulfuras"
     (describe "when sell by date is not in the limit"
       (it "returns the maximum quality for Sulfuras and same sell by date"
-        (let [item-1 (item "Sulfuras, Hand of Ragnaros" 10 25)
+        (let [item-1 (item SULFURAS 10 25)
               updated-items (update-quality [item-1])]
-          (should= {:name "Sulfuras, Hand of Ragnaros" :sell-in 10 :quality 80} (first updated-items)))
+          (should= {:name SULFURAS :sell-in 10 :quality SULFURAS_QUALITY_MAX_LIMIT} (first updated-items)))
       )
     )
     (describe "when sell by date is in the limit"
       (it "returns the maximum quality for Sulfuras and same sell by date"
-        (let [item-1 (item "Sulfuras, Hand of Ragnaros" 0 25)
+        (let [item-1 (item SULFURAS 0 25)
               updated-items (update-quality [item-1])]
-          (should= {:name "Sulfuras, Hand of Ragnaros" :sell-in 0 :quality 80} (first updated-items)))
+          (should= {:name SULFURAS :sell-in 0 :quality SULFURAS_QUALITY_MAX_LIMIT} (first updated-items)))
       )
     )
   )
-  (describe "Aged Brie"
+  (describe AGED_BRIE
     (describe "when sell by date is not in the limit"
       (describe "when quality is not in the limit"
         (it "returns more one of quality and sell by date"
-          (let [item-1 (item "Aged Brie" 10 25)
+          (let [item-1 (item AGED_BRIE 10 25)
                 updated-items (update-quality [item-1])]
-            (should= {:name "Aged Brie" :sell-in 9 :quality 26} (first updated-items)))
+            (should= {:name AGED_BRIE :sell-in 9 :quality 26} (first updated-items)))
         )
       )
       (describe "when quality is the minimum limit"
         (it "returns less one of sell by date and one more of quality"
-          (let [item-1 (item "Aged Brie" 10 0)
+          (let [item-1 (item AGED_BRIE 10 0)
                 updated-items (update-quality [item-1])]
-            (should= {:name "Aged Brie" :sell-in 9 :quality 1} (first updated-items)))
+            (should= {:name AGED_BRIE :sell-in 9 :quality 1} (first updated-items)))
         )
       )
       (describe "when quality is the maximum limit"
         (it "returns maximum limit of quality and sell by date"
-          (let [item-1 (item "Aged Brie" 10 50)
+          (let [item-1 (item AGED_BRIE 10 QUALITY_MAX_LIMIT)
                 updated-items (update-quality [item-1])]
-            (should= {:name "Aged Brie" :sell-in 9 :quality 50} (first updated-items)))
+            (should= {:name AGED_BRIE :sell-in 9 :quality QUALITY_MAX_LIMIT} (first updated-items)))
         )
       )
     )
     (describe "when sell by date is in the limit"
       (describe "when quality is not in the limit"
         (it "returns two more of quality and less one of sell by date"
-          (let [item-1 (item "Aged Brie" 0 25)
+          (let [item-1 (item AGED_BRIE 0 25)
                 updated-items (update-quality [item-1])]
-            (should= {:name "Aged Brie" :sell-in -1 :quality 27} (first updated-items)))
+            (should= {:name AGED_BRIE :sell-in -1 :quality 27} (first updated-items)))
         )
       )
       (describe "when quality is the minimum limit"
         (it "returns less one of sell by date and two more of quality"
-          (let [item-1 (item "Aged Brie" 0 0)
+          (let [item-1 (item AGED_BRIE 0 0)
                 updated-items (update-quality [item-1])]
-            (should= {:name "Aged Brie" :sell-in -1 :quality 2} (first updated-items)))
+            (should= {:name AGED_BRIE :sell-in -1 :quality 2} (first updated-items)))
         )
       )
       (describe "when quality is the maximum limit"
         (it "returns the maximum limit of quality and less one of sell by date"
-          (let [item-1 (item "Aged Brie" 0 50)
+          (let [item-1 (item AGED_BRIE 0 QUALITY_MAX_LIMIT)
                 updated-items (update-quality [item-1])]
-            (should= {:name "Aged Brie" :sell-in -1 :quality 50} (first updated-items)))
+            (should= {:name AGED_BRIE :sell-in -1 :quality QUALITY_MAX_LIMIT} (first updated-items)))
         )
       )
       (describe "when quality is one less the maximum limit"
         (it "returns the maximum limit of quality and less one of sell by date"
-          (let [item-1 (item "Aged Brie" 0 49)
+          (let [item-1 (item AGED_BRIE 0 (- QUALITY_MAX_LIMIT 1))
                 updated-items (update-quality [item-1])]
-            (should= {:name "Aged Brie" :sell-in -1 :quality 50} (first updated-items)))
+            (should= {:name AGED_BRIE :sell-in -1 :quality QUALITY_MAX_LIMIT} (first updated-items)))
         )
       )
     )
   )
-  (describe "Backstage passes to a TAFKAL80ETC concert"
+  (describe BACKSTAGE
     (describe "when sell by date is not in the limit"
       (describe "when quality is not in the limit"
         (it "returns more one of quality and sell by date"
-          (let [item-1 (item "Backstage passes to a TAFKAL80ETC concert" 20 25)
+          (let [item-1 (item BACKSTAGE 20 25)
                 updated-items (update-quality [item-1])]
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 19 :quality 26} (first updated-items)))
+            (should= {:name BACKSTAGE :sell-in 19 :quality 26} (first updated-items)))
         )
       )
       (describe "when quality is the minimum limit"
         (it "returns less one of sell by date and one more of quality"
-          (let [item-1 (item "Backstage passes to a TAFKAL80ETC concert" 20 0)
+          (let [item-1 (item BACKSTAGE 20 0)
                 updated-items (update-quality [item-1])]
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 19 :quality 1} (first updated-items)))
+            (should= {:name BACKSTAGE :sell-in 19 :quality 1} (first updated-items)))
         )
       )
       (describe "when quality is the maximum limit"
         (it "returns maximum limit of quality and sell by date"
-          (let [item-1 (item "Backstage passes to a TAFKAL80ETC concert" 20 50)
+          (let [item-1 (item BACKSTAGE 20 QUALITY_MAX_LIMIT)
                 updated-items (update-quality [item-1])]
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 19 :quality 50} (first updated-items)))
+            (should= {:name BACKSTAGE :sell-in 19 :quality QUALITY_MAX_LIMIT} (first updated-items)))
         )
       )
     )
     (describe "when sell by date is in the limit"
       (describe "when quality is not in the limit"
         (it "returns the minimum limit of quality and less one of sell by date"
-          (let [item-1 (item "Backstage passes to a TAFKAL80ETC concert" 0 25)
+          (let [item-1 (item BACKSTAGE 0 25)
                 updated-items (update-quality [item-1])]
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in -1 :quality 0} (first updated-items)))
+            (should= {:name BACKSTAGE :sell-in -1 :quality 0} (first updated-items)))
         )
       )
       (describe "when quality is the minimum limit"
         (it "returns less one of sell by date and the minimum limit of quality"
-          (let [item-1 (item "Backstage passes to a TAFKAL80ETC concert" 0 0)
+          (let [item-1 (item BACKSTAGE 0 0)
                 updated-items (update-quality [item-1])]
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in -1 :quality 0} (first updated-items)))
+            (should= {:name BACKSTAGE :sell-in -1 :quality 0} (first updated-items)))
         )
       )
       (describe "when quality is the maximum limit"
         (it "returns the minimum limit of quality and less one of sell by date"
-          (let [item-1 (item "Backstage passes to a TAFKAL80ETC concert" 0 50)
+          (let [item-1 (item BACKSTAGE 0 QUALITY_MAX_LIMIT)
                 updated-items (update-quality [item-1])]
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in -1 :quality 0} (first updated-items)))
+            (should= {:name BACKSTAGE :sell-in -1 :quality 0} (first updated-items)))
         )
       )
     )
     (describe "when sell by date is less or equal 10 and more than 5"
       (describe "when quality is not in the limit"
         (it "returns two more of quality and less one of sell by date"
-          (let [item-1 (item "Backstage passes to a TAFKAL80ETC concert" 10 25)
-                item-2 (item "Backstage passes to a TAFKAL80ETC concert" 6 29)
+          (let [item-1 (item BACKSTAGE 10 25)
+                item-2 (item BACKSTAGE 6 29)
                 updated-items (update-quality [item-1 item-2])]
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 9 :quality 27} (first updated-items))
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 5 :quality 31} (second updated-items)))
+            (should= {:name BACKSTAGE :sell-in 9 :quality 27} (first updated-items))
+            (should= {:name BACKSTAGE :sell-in 5 :quality 31} (second updated-items)))
         )
       )
       (describe "when quality is the minimum limit"
         (it "returns less one of sell by date and two more of quality"
-          (let [item-1 (item "Backstage passes to a TAFKAL80ETC concert" 10 0)
-                item-2 (item "Backstage passes to a TAFKAL80ETC concert" 6 0)
+          (let [item-1 (item BACKSTAGE 10 0)
+                item-2 (item BACKSTAGE 6 0)
                 updated-items (update-quality [item-1 item-2])]
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 9 :quality 2} (first updated-items))
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 5 :quality 2} (second updated-items)))
+            (should= {:name BACKSTAGE :sell-in 9 :quality 2} (first updated-items))
+            (should= {:name BACKSTAGE :sell-in 5 :quality 2} (second updated-items)))
         )
       )
       (describe "when quality is the maximum limit"
         (it "returns the maximum limit of quality and less one of sell by date"
-          (let [item-1 (item "Backstage passes to a TAFKAL80ETC concert" 10 50)
-                item-2 (item "Backstage passes to a TAFKAL80ETC concert" 6 50)
+          (let [item-1 (item BACKSTAGE 10 QUALITY_MAX_LIMIT)
+                item-2 (item BACKSTAGE 6 QUALITY_MAX_LIMIT)
                 updated-items (update-quality [item-1 item-2])]
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 9 :quality 50} (first updated-items))
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 5 :quality 50} (second updated-items)))
+            (should= {:name BACKSTAGE :sell-in 9 :quality QUALITY_MAX_LIMIT} (first updated-items))
+            (should= {:name BACKSTAGE :sell-in 5 :quality QUALITY_MAX_LIMIT} (second updated-items)))
         )
       )
       (describe "when quality is one less than the maximum limit"
         (it "returns the maximum limit of quality and less one of sell by date"
-          (let [item-1 (item "Backstage passes to a TAFKAL80ETC concert" 10 49)
-                item-2 (item "Backstage passes to a TAFKAL80ETC concert" 6 49)
+          (let [item-1 (item BACKSTAGE 10 (- QUALITY_MAX_LIMIT 1))
+                item-2 (item BACKSTAGE 6 (- QUALITY_MAX_LIMIT 1))
                 updated-items (update-quality [item-1 item-2])]
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 9 :quality 50} (first updated-items))
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 5 :quality 50} (second updated-items)))
+            (should= {:name BACKSTAGE :sell-in 9 :quality QUALITY_MAX_LIMIT} (first updated-items))
+            (should= {:name BACKSTAGE :sell-in 5 :quality QUALITY_MAX_LIMIT} (second updated-items)))
         )
       )
     )
     (describe "when sell by date is less or equal 5 and more than 0"
       (describe "when quality is not in the limit"
         (it "returns three more of quality and less one of sell by date"
-          (let [item-1 (item "Backstage passes to a TAFKAL80ETC concert" 5 25)
-                item-2 (item "Backstage passes to a TAFKAL80ETC concert" 1 29)
+          (let [item-1 (item BACKSTAGE 5 25)
+                item-2 (item BACKSTAGE 1 29)
                 updated-items (update-quality [item-1 item-2])]
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 4 :quality 28} (first updated-items))
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 0 :quality 32} (second updated-items)))
+            (should= {:name BACKSTAGE :sell-in 4 :quality 28} (first updated-items))
+            (should= {:name BACKSTAGE :sell-in 0 :quality 32} (second updated-items)))
         )
       )
       (describe "when quality is the minimum limit"
         (it "returns less one of sell by date and three more of quality"
-          (let [item-1 (item "Backstage passes to a TAFKAL80ETC concert" 5 0)
-                item-2 (item "Backstage passes to a TAFKAL80ETC concert" 1 0)
+          (let [item-1 (item BACKSTAGE 5 0)
+                item-2 (item BACKSTAGE 1 0)
                 updated-items (update-quality [item-1 item-2])]
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 4 :quality 3} (first updated-items))
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 0 :quality 3} (second updated-items)))
+            (should= {:name BACKSTAGE :sell-in 4 :quality 3} (first updated-items))
+            (should= {:name BACKSTAGE :sell-in 0 :quality 3} (second updated-items)))
         )
       )
       (describe "when quality is the maximum limit"
         (it "returns the maximum limit of quality and less one of sell by date"
-          (let [item-1 (item "Backstage passes to a TAFKAL80ETC concert" 5 50)
-                item-2 (item "Backstage passes to a TAFKAL80ETC concert" 1 50)
+          (let [item-1 (item BACKSTAGE 5 QUALITY_MAX_LIMIT)
+                item-2 (item BACKSTAGE 1 QUALITY_MAX_LIMIT)
                 updated-items (update-quality [item-1 item-2])]
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 4 :quality 50} (first updated-items))
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 0 :quality 50} (second updated-items)))
+            (should= {:name BACKSTAGE :sell-in 4 :quality QUALITY_MAX_LIMIT} (first updated-items))
+            (should= {:name BACKSTAGE :sell-in 0 :quality QUALITY_MAX_LIMIT} (second updated-items)))
         )
       )
       (describe "when quality is one less than the maximum limit"
         (it "returns the maximum limit of quality and less one of sell by date"
-          (let [item-1 (item "Backstage passes to a TAFKAL80ETC concert" 5 49)
-                item-2 (item "Backstage passes to a TAFKAL80ETC concert" 1 49)
+          (let [item-1 (item BACKSTAGE 5 (- QUALITY_MAX_LIMIT 1))
+                item-2 (item BACKSTAGE 1 (- QUALITY_MAX_LIMIT 1))
                 updated-items (update-quality [item-1 item-2])]
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 4 :quality 50} (first updated-items))
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 0 :quality 50} (second updated-items)))
+            (should= {:name BACKSTAGE :sell-in 4 :quality QUALITY_MAX_LIMIT} (first updated-items))
+            (should= {:name BACKSTAGE :sell-in 0 :quality QUALITY_MAX_LIMIT} (second updated-items)))
         )
       )
       (describe "when quality is two less than the maximum limit"
         (it "returns the maximum limit of quality and less one of sell by date"
-          (let [item-1 (item "Backstage passes to a TAFKAL80ETC concert" 5 48)
-                item-2 (item "Backstage passes to a TAFKAL80ETC concert" 1 48)
+          (let [item-1 (item BACKSTAGE 5 (- QUALITY_MAX_LIMIT 2))
+                item-2 (item BACKSTAGE 1 (- QUALITY_MAX_LIMIT 2))
                 updated-items (update-quality [item-1 item-2])]
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 4 :quality 50} (first updated-items))
-            (should= {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 0 :quality 50} (second updated-items)))
+            (should= {:name BACKSTAGE :sell-in 4 :quality QUALITY_MAX_LIMIT} (first updated-items))
+            (should= {:name BACKSTAGE :sell-in 0 :quality QUALITY_MAX_LIMIT} (second updated-items)))
         )
       )
     )
---
+++ clojure/src/gilded_rose/constants.clj
@@ -0,0 +1,10 @@
+(ns gilded-rose.constants)
+
+(def ^:const SULFURAS "Sulfuras, Hand of Ragnaros")
+(def ^:const AGED_BRIE "Aged Brie")
+(def ^:const BACKSTAGE "Backstage passes to a TAFKAL80ETC concert")
+
+(def ^:const SELL_IN_LIMIT 0)
+(def ^:const QUALITY_MIN_LIMIT 0)
+(def ^:const QUALITY_MAX_LIMIT 50)
+(def ^:const SULFURAS_QUALITY_MAX_LIMIT 80)
